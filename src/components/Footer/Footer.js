f3de0e36e3400905a096791e35d834ee93a96044
forid1026 <sheikhforid1026@gmai.com>
2022-09-22 11:50:24 +0600

    added bootstrap navbar

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p> &copy; copyright by rest countries api. All right reserved. </p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,19 +3,31 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="main-menu">
-      <div className="logo">
-      <a href="#">
-        <h2>Farid</h2>
-      </a>
-      </div>
-      <div className="menu-list">
-        <a href="/home">Home</a>
-        <a href="/about">About</a>
-        <a href="/service">Service</a>
-        <a href="/blog">Blog</a>
-        <a href="/contact">Contact</a>
-      </div>
+    <div>
+      <nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
+  <div class="container">
+    <a class="navbar-brand text-uppercase" href="/home">Rest Countries</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0 text-uppercase">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/Portfolio">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
   );
 };
